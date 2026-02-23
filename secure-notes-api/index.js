f7b5d707100224f5f6d9066cd474427d2b9623f8
Petros <petros.pieska@outlook.com>
2026-02-23 11:46:24 +0200

    Fix port for Render

--- secure-notes-api/index.js
+++ secure-notes-api/index.js
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/notes", notesRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
