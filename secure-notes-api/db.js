f4e2a8bda91ae7de32f61577448985dfd10ab420
Petros <petros.pieska@outlook.com>
2026-02-23 13:07:25 +0200

    Fix DB connection

--- secure-notes-api/db.js
+++ secure-notes-api/db.js
@@ -1,11 +1,24 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "secure_notes",
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Production (Render + Neon)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Local development
+  pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "secure_notes",
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+  });
+}
 
 module.exports = pool;
